47d6563596697435becbb663f4101a43b3d1b47d
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2025-12-07 21:30:51 +0700

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+const API_BASE_URL = 'https://coffee-backend-1.onrender.com/api/v1';
 
 // Fetch all coffee products
 export const fetchCoffeeProducts = async () => {
